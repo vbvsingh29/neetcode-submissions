5701385c097e42df7a6407d9415daff0f03a3cc5
Vaibhav Singh <110039590+vbvsingh29@users.noreply.github.com>
2026-07-13 08:09:11 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const map = new Map();
+        for (const n of nums) {
+            if (map.has(n)) return true;
+            else map.set(n,1)
+        }
+        return false;
+    }
+}
