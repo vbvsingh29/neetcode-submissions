d50cb29ce3104bd04b28a484e1ff3d720d937b06
Vaibhav Singh <110039590+vbvsingh29@users.noreply.github.com>
2026-07-13 08:46:26 +0530

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+      const map = new Map();
+      for(const str of strs){
+      const count = new Array(26).fill(0);
+       for(const char of str){
+        count[char.charCodeAt(0)-97]++
+       }
+        const key = count.join('#');
+
+        if(!map.has(key)){
+            map.set(key ,[])
+        }
+
+        map.get(key).push(str)
+      }
+      return Array.from(map.values())
+    }
+}
